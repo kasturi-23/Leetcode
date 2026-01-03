10c4d04a89c3439cbe166a09ca984d1f96336557
Kasturi Pandurang Shinde <86947623+kasturi-23@users.noreply.github.com>
2026-01-03 22:15:06 +0530

    Time: 0 ms (100%), Space: 46.7 MB (7.04%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] arr = new int[2*n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            arr[count]= nums[i];
+            arr[count+1] =nums[n+i];
+            count=count+2;
+        }
+        return arr;
+    }
+}

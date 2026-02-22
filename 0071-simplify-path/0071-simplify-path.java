10fc3f07bd4b90b220d05795af72171b572400b9
Kasturi Pandurang Shinde <86947623+kasturi-23@users.noreply.github.com>
2026-02-22 17:03:59 -0600

    Time: 4 ms (95.59%), Space: 45 MB (66.8%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public String simplifyPath(String path) {
+
+        Stack<String> st = new Stack<>();
+
+        String[] parts = path.split("/");
+
+        for(String part : parts){
+
+            if(part.equals("") || part.equals(".")){
+                continue;
+            }
+
+            else if(part.equals("..")){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }
+
+            else{
+                st.push(part);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(String dir : st){
+            sb.append("/");
+            sb.append(dir);
+        }
+
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+}
